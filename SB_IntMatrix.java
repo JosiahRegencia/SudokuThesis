2688cb10adc3d3b16009fa29bcdbc67bac81385c
JosiahRegencia <jawshaeleazar@gmail.com>
2017-07-12 12:22:24 +0800

    SB_IntMatrix

--- SB_IntMatrix.java
+++ SB_IntMatrix.java
@@ -2,16 +2,19 @@
 
 public class SB_IntMatrix extends SudokuBoard
 {
+  int [][] m;
   public SB_IntMatrix()
   {
   }
   
   public int get(int r, int c)
   {
+    return m[r-1][c-1]
   }
   
   public void set(int r, int c, int v)
   {
+    m[r-1][c-1] = v;
   }  
   
   boolean isRowCompatible(int r, int c)
